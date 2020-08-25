0a684fdc169bff002c4ac31ac2949ec8982fd567
yta <ytasegundo@gmail.com>
2020-08-25 14:40:31 -0400

    botones de eleccion html

--- ciclo.js
+++ ciclo.js
@@ -1,4 +1,11 @@
 //variables
+var uno = document.getElementById("uno");
+var dos = document.getElementById("dos");
+var tres = document.getElementById("tres");
+var cuatro = document.getElementById("cuatro");
+var cinco = document.getElementById("cinco");
+var seis = document. getElementById("seis");
+
 var number = document.getElementById("number");
 var white = document.getElementById("white");
 var math = document.getElementById("math");
@@ -11,12 +18,27 @@ var name = document.getElementById("name");
 var result = document.getElementById("result");
 
 // pedir numero de 1 a 100 y mostrar desde el uno hasta numero seleccionado
-
+uno.onclick = function(e){
+ alert("estamos trabajando para tiiiii");
+}
 
 // respuesta esperada blanco si no volver a preguntar
-
+dos.onclick = function(e){
+    alert("en proceso");
+}
 // pedir notas y promediar matematicas ciencias fisica
-
+tres.onclick = function(e){
+    alert("en proceso");
+}
 //pedir 3 frutas de manera individual e imprimir todas juntas excepto "manzana"
-
+cuatro.onclick = function(e){
+    alert("en proceso");
+}
 //pedir nombre y separar vocales de consonantes y mostrar con un alert este resultado
+cinco.onclick = function(e){
+    alert("en proceso");
+}
+
+seis.onclick = function(e){
+    alert("selecciona otra opción");
+}
