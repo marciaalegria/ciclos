477574b0413f4f8e0f6a5f42b27d9512ec6833a6
yta <ytasegundo@gmail.com>
2020-08-25 14:05:53 -0400

    estructura html y variables en js

---
+++ ciclo.js
@@ -0,0 +1,22 @@
+//variables
+var number = document.getElementById("number");
+var white = document.getElementById("white");
+var math = document.getElementById("math");
+var ciencias = document.getElementById("ciencias");
+var fisica = document.getElementById("fisica");
+var fruit_1 = document.getElementById("fruit_1");
+var fruit_2 = document.getElementById("fruit_2");
+var fruit_3 = document.getElementById("fruit_3");
+var name = document.getElementById("name");
+var result = document.getElementById("result");
+
+// pedir numero de 1 a 100 y mostrar desde el uno hasta numero seleccionado
+
+
+// respuesta esperada blanco si no volver a preguntar
+
+// pedir notas y promediar matematicas ciencias fisica
+
+//pedir 3 frutas de manera individual e imprimir todas juntas excepto "manzana"
+
+//pedir nombre y separar vocales de consonantes y mostrar con un alert este resultado
